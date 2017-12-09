8ad9e39ce2f9c91e76411fc88987f9364ed0ca14
Newsof <suhov-p@bk.ru>
2017-12-09 19:30:04 +0300

    OK

--- app/controllers/armstrong_controller.rb
+++ app/controllers/armstrong_controller.rb
@@ -1,21 +1,25 @@
 class ArmstrongController < ApplicationController
-  def input
-  end
+  def input; end
 
   def view
     @num = params[:a].to_i
     @num1 = params[:b].to_i
-    @res = []
-    @count = 0
-    (@num..@num1).each do |num|
-      m = num.to_s.split(//)
-      s = num.to_s.size
-      sc = 0
-      s.times { |i| sc += m[i].to_i**s }
-      if num.to_i == sc
-        @res << sc
-        @count += 1
+    if res = ArmstrongResult.find_by_a_and_b(@num, @num1)
+      @result = ActiveSupport::JSON.decode(res.result)
+    else
+      @ress = (@num..@num1).select do |num|
+        chars = num.to_s.chars
+        num == chars.sum { |e| e.to_i**chars.size }
       end
+      @count = @ress.size
+      @result = @ress
+      res = ArmstrongResult.create a: @num, b: @num1, result: ActiveSupport::JSON.encode(@result)
+      res.save
     end
+  end
+
+  def results
+    @result = ArmstrongResult.all
+    render xml: @result
   end
 end
